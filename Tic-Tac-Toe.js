7568e11a08ca8396d70d2854834ec3e4d9f6a34c
Nikhil singh <152882476+NikhilSingh750@users.noreply.github.com>
2024-02-21 23:06:20 +0530

    Update Tic-Tac-Toe.js
    updated file

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,61 +1,69 @@
-let turn = 'X'
+let turn = 'X';
 let gameover = false;
 
 
-const changeturn =()=>{
-    return turn == 'X'?'O':'X'
+const changeturn = () => {
+    return turn == 'X' ? 'O' : 'X'
 }
 
-const checkwinner = ()=>{
+const checkwinner = () => {
     let box = document.getElementsByClassName('boxtext');
-const winingcombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [8,5,2],
-    [0,4,8],
-    [2,4,6]
-  ]
-  winingcombos.forEach(e =>{
-    if ((box[e[0]].innerText === box[e[1]].innerText) && (box[e[1]].innerText === box[e[2]].innerText) && (box[e[0]].innerText !== "") )
-    {
-        document.querySelector('.info').innerText = box[e[0]].innerText+' is won'
-        gameover = true
-    }
-  })
+    const winingcombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [8, 5, 2],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    if(gameover == false){
 
+        winingcombos.forEach(e => {
+            if ((box[e[0]].innerText === box[e[1]].innerText) && (box[e[1]].innerText === box[e[2]].innerText) && (box[e[0]].innerText !== "")) {
+                let a = box[e[0]].innerText;
+                document.querySelector('.info').innerText = a  + ' is won'
+                gameover = true
+                
+                
+            }
+        })
+        
+    }
 }
 
 
 const boxes = document.getElementsByClassName('box');
-Array.from(boxes).forEach(element=>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext')
-    element.addEventListener('click',()=>{
-        boxtext.innerText = turn;
-       turn = changeturn()
-        checkwinner()
-        if(gameover == false)
-        {
-            document.querySelector('.info').innerText = 'Turn for '+turn
+    element.addEventListener('click', () => {
+        if (boxtext.innerText == "") {
+
+            boxtext.innerText = turn;
+            turn = changeturn()
+            checkwinner()
+            if (gameover == false) {
+                document.querySelector('.info').innerText = 'Turn for ' + turn
+            }
+
         }
     })
 })
 
 let btn = document.getElementById('reset');
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
     let boxes = document.querySelectorAll('.boxtext');
-    Array.from(boxes).forEach(element=>{
+    Array.from(boxes).forEach(element => {
         element.innerText = ""
     })
     turn = 'X'
     gameover = false
     document.querySelector('.info').innerText = 'Turn for ' + turn
-    
+
 })
 
 let playbtn = document.getElementById('buttonplay');
-playbtn.addEventListener('click',()=>{
+playbtn.addEventListener('click', () => {
     location.href = 'Tic-Tac-Toe.html'
 })
